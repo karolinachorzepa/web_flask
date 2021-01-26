c504fb1bd16a9f7ee0f1bf7efbbd79dce8a5f522
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2021-01-26 20:46:13 +0100

    flaskworks

--- appflask.py
+++ appflask.py
@@ -3,6 +3,8 @@ from flask import request, redirect, render_template
 
 app2=Flask(__name__)
 
+@app2.route('/')
+
 @app2.route('/mypage/me', methods=['GET'])
 def about_me():
     return render_template("1stside.html")
@@ -11,12 +13,16 @@ def about_me():
 def contact_me():
     return render_template("2ndside.html")
 
-@app2.route('/mypage/contactmeform', methods=['GET','POST'])
+@app2.route('/mypage/contactcontactmeform', methods=['GET'])
 def contact_me_form():
+    return render_template("3rdside.html")
+
+@app2.route('/mypage/contactmeform', methods=['POST'])
+def contact_me_formmessage():
     if request.method=='POST':
         print(request.form)
-        return redirect("/mypage/contactmeform")
-    return render_template("3rdside.html")
+    return render_template("3rdhtml.html")
+
         
 if __name__ == "__main__":
     app2.run()
